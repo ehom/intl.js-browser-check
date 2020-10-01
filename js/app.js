3bc15b46e18ab459022aef11e8dd6d56569d2052
ehom <erwin.hom@puppet.com>
2020-10-01 12:00:47 -0700

    Remove use of babel <script>. Add npm project

--- js/app.js
+++ js/app.js
@@ -1,71 +1,87 @@
-const supportedFunctions = Object.getOwnPropertyNames(window["Intl"]);
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
-const isSupported = (functionName) => { 
+var supportedFunctions = Object.getOwnPropertyNames(window["Intl"]);
+
+var isSupported = function isSupported(functionName) {
   return supportedFunctions.indexOf(functionName) > -1;
 };
 
 function FunctionLink(props) {
-  const URL = `https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/${props.name}`;
-  const title = `Link to MDN doc on Intl.${props.name}`;
-  return (
-    <a href={URL} target="_blank" title={title}>{props.name}</a>
+  var URL = "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/" + props.name;
+  var title = "Link to MDN doc on Intl." + props.name;
+  return React.createElement(
+    "a",
+    { href: URL, target: "_blank", title: title },
+    props.name
   );
 }
 
 function IntlJsSupport() {
-  const functionNames = [
-    "DateTimeFormat",
-    "NumberFormat", 
-    "RelativeTimeFormat",
-    "Collator",
-    "ListFormat",
-    "PluralRules",
-    "Locale",
-    "getCanonicalLocales"
-  ];
-  
+  var functionNames = ["DateTimeFormat", "NumberFormat", "RelativeTimeFormat", "Collator", "ListFormat", "PluralRules", "Locale", "getCanonicalLocales"];
+
   console.debug("functionNames:", functionNames);
-  
-  const buildSupportTable = (names) => {
-    const CHECK_MARK = 0x2714;
-    return functionNames.reduce((obj, name) => {
-      return {
-        ...obj, [name]: isSupported(name) ? String.fromCharCode(CHECK_MARK) : ''
-      };
+
+  var buildSupportTable = function buildSupportTable(names) {
+    var CHECK_MARK = 0x2714;
+    return functionNames.reduce(function (obj, name) {
+      return Object.assign({}, obj, _defineProperty({}, name, isSupported(name) ? String.fromCharCode(CHECK_MARK) : ''));
     }, {});
   };
 
-  let table = buildSupportTable(functionNames);
+  var table = buildSupportTable(functionNames);
 
-  const tableRows = Object.keys(table).map((item) => 
-    <tr>
-       <td><FunctionLink name={item}/></td>
-       <td class="text-center">{table[item]}</td>
-    </tr>
-  );
-    
-  return (
-    <table class="table table-md table-hover">
-      <thead>
-        <th>Intl Function Name</th>
-        <th class="text-center">Supported</th>
-      </thead>
-      <tbody>{tableRows}</tbody>
-    </table>
+  var tableRows = Object.keys(table).map(function (item) {
+    return React.createElement(
+      "tr",
+      null,
+      React.createElement(
+        "td",
+        null,
+        React.createElement(FunctionLink, { name: item })
+      ),
+      React.createElement(
+        "td",
+        { "class": "text-center" },
+        table[item]
+      )
+    );
+  });
+
+  return React.createElement(
+    "table",
+    { "class": "table table-md table-hover" },
+    React.createElement(
+      "thead",
+      null,
+      React.createElement(
+        "th",
+        null,
+        "Intl Function Name"
+      ),
+      React.createElement(
+        "th",
+        { "class": "text-center" },
+        "Supported"
+      )
+    ),
+    React.createElement(
+      "tbody",
+      null,
+      tableRows
+    )
   );
 }
 
 function BrowserInfo() {
-  return (
-    <React.Fragment>
-      ({navigator.userAgent})
-    </React.Fragment>
+  return React.createElement(
+    React.Fragment,
+    null,
+    "(",
+    navigator.userAgent,
+    ")"
   );
 }
 
-ReactDOM.render(<BrowserInfo/>, document.getElementById('browserInfo'));
+ReactDOM.render(React.createElement(BrowserInfo, null), document.getElementById('browserInfo'));
 
-ReactDOM.render(
-  <IntlJsSupport/>,
-  document.getElementById('main')
-);
+ReactDOM.render(React.createElement(IntlJsSupport, null), document.getElementById('main'));
