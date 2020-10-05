9493f8559f3baa44844168d592bed983cc60a00e
ehom <runner@fv-az87.faoygvci0gyu3pyzw0cq1w1u2a.bx.internal.cloudapp.net>
2020-10-05 21:09:34 +0000

    update generated file

--- js/app.js
+++ js/app.js
@@ -6,6 +6,28 @@ var isSupported = function isSupported(functionName) {
   return supportedFunctions.indexOf(functionName) > -1;
 };
 
+var page = React.createElement(
+  React.Fragment,
+  null,
+  React.createElement(
+    "div",
+    { className: "jumbotron pb-2" },
+    React.createElement(
+      "h3",
+      { className: "pb-3" },
+      "Intl.js support in your browser"
+    ),
+    React.createElement(BrowserInfo, null)
+  ),
+  React.createElement(
+    "div",
+    { className: "container" },
+    React.createElement(IntlJsSupport, null)
+  )
+);
+
+ReactDOM.render(page, document.getElementById('root'));
+
 function FunctionLink(props) {
   var URL = "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/" + props.name;
   var title = "Link to MDN doc on Intl." + props.name;
@@ -21,28 +43,28 @@ function IntlJsSupport() {
 
   console.debug("functionNames:", functionNames);
 
-  var buildSupportTable = function buildSupportTable(names) {
+  var buildReport = function buildReport(names) {
     var CHECK_MARK = 0x2714;
     return functionNames.reduce(function (obj, name) {
       return Object.assign({}, obj, _defineProperty({}, name, isSupported(name) ? String.fromCharCode(CHECK_MARK) : ''));
     }, {});
   };
 
-  var table = buildSupportTable(functionNames);
+  var report = buildReport(functionNames);
 
-  var tableRows = Object.keys(table).map(function (item) {
+  var tableRows = Object.keys(report).map(function (entry) {
     return React.createElement(
       "tr",
       null,
       React.createElement(
         "td",
         null,
-        React.createElement(FunctionLink, { name: item })
+        React.createElement(FunctionLink, { name: entry })
       ),
       React.createElement(
         "td",
         { "class": "text-center" },
-        table[item]
+        report[entry]
       )
     );
   });
@@ -74,14 +96,8 @@ function IntlJsSupport() {
 
 function BrowserInfo() {
   return React.createElement(
-    React.Fragment,
-    null,
-    "(",
-    navigator.userAgent,
-    ")"
+    "div",
+    { "class": "alert alert-light", role: "alert" },
+    navigator.userAgent
   );
 }
-
-ReactDOM.render(React.createElement(BrowserInfo, null), document.getElementById('browserInfo'));
-
-ReactDOM.render(React.createElement(IntlJsSupport, null), document.getElementById('main'));
