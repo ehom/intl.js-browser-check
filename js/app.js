d490d7faafbc02796631791ba42260c18d004a8f
ehom <ehom@acm.org>
2021-01-15 21:38:28 +0000

    update generated file

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
-var intljsTable = {
-  support: {},
-  nodejs: {}
-};
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 var supportedFunctions = Object.getOwnPropertyNames(window["Intl"]);
 
@@ -9,99 +12,123 @@ var isSupported = function isSupported(functionName) {
   return supportedFunctions.indexOf(functionName) > -1;
 };
 
-fetch("https://raw.githubusercontent.com/ehom/nodejs-intl/main/intljs.json").then(function (response) {
-  return response.json();
-}).then(function (data) {
-  console.debug("nodejs:", data);
-  var _iteratorNormalCompletion = true;
-  var _didIteratorError = false;
-  var _iteratorError = undefined;
-
-  try {
-    for (var _iterator = data["Intl.js"][Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-      var name = _step.value;
-
-      intljsTable.support[name] = { browser: false, nodejs: true };
-    }
-  } catch (err) {
-    _didIteratorError = true;
-    _iteratorError = err;
-  } finally {
-    try {
-      if (!_iteratorNormalCompletion && _iterator.return) {
-        _iterator.return();
-      }
-    } finally {
-      if (_didIteratorError) {
-        throw _iteratorError;
-      }
-    }
-  }
-
-  intljsTable.nodejs["version"] = data["Node.js"].version;
-  console.log("nodejs version", intljsTable.nodejs.version);
-  main();
-});
-
 var APP_NAME = "Intl.js Support";
-
 document.title = APP_NAME;
 
-function main() {
-  var _iteratorNormalCompletion2 = true;
-  var _didIteratorError2 = false;
-  var _iteratorError2 = undefined;
+var App = function (_React$Component) {
+  _inherits(App, _React$Component);
 
-  try {
-    for (var _iterator2 = Object.keys(intljsTable.support)[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
-      var key = _step2.value;
+  function App(props) {
+    _classCallCheck(this, App);
 
-      intljsTable.support[key].browser = isSupported(key);
-    }
-  } catch (err) {
-    _didIteratorError2 = true;
-    _iteratorError2 = err;
-  } finally {
-    try {
-      if (!_iteratorNormalCompletion2 && _iterator2.return) {
-        _iterator2.return();
+    var _this = _possibleConstructorReturn(this, (App.__proto__ || Object.getPrototypeOf(App)).call(this, props));
+
+    _this.state = {
+      intljs: {},
+      nodejs: {}
+    };
+    fetch("https://raw.githubusercontent.com/ehom/nodejs-intl/main/intljs.json").then(function (response) {
+      return response.json();
+    }).then(function (data) {
+      console.debug("nodejs:", data);
+
+      intljsInfo = {};
+      nodejsInfo = {};
+
+      var _iteratorNormalCompletion = true;
+      var _didIteratorError = false;
+      var _iteratorError = undefined;
+
+      try {
+        for (var _iterator = data["Intl.js"][Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+          var name = _step.value;
+
+          intljsInfo[name] = { browser: false, nodejs: true };
+        }
+      } catch (err) {
+        _didIteratorError = true;
+        _iteratorError = err;
+      } finally {
+        try {
+          if (!_iteratorNormalCompletion && _iterator.return) {
+            _iterator.return();
+          }
+        } finally {
+          if (_didIteratorError) {
+            throw _iteratorError;
+          }
+        }
       }
-    } finally {
-      if (_didIteratorError2) {
-        throw _iteratorError2;
+
+      nodejsInfo["version"] = data["Node.js"].version;
+      var _iteratorNormalCompletion2 = true;
+      var _didIteratorError2 = false;
+      var _iteratorError2 = undefined;
+
+      try {
+        for (var _iterator2 = Object.keys(intljsInfo)[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
+          var key = _step2.value;
+
+          intljsInfo[key].browser = isSupported(key);
+        }
+      } catch (err) {
+        _didIteratorError2 = true;
+        _iteratorError2 = err;
+      } finally {
+        try {
+          if (!_iteratorNormalCompletion2 && _iterator2.return) {
+            _iterator2.return();
+          }
+        } finally {
+          if (_didIteratorError2) {
+            throw _iteratorError2;
+          }
+        }
       }
-    }
+
+      console.debug("updated: ", intljsInfo);
+      _this.setState({
+        intljs: intljsInfo,
+        nodejs: nodejsInfo
+      });
+    });
+    return _this;
   }
 
-  console.debug("updated: ", intljsTable.support);
-
-  var App = function App() {
-    return React.createElement(
-      React.Fragment,
-      null,
-      React.createElement(
-        "div",
-        { className: "jumbotron pt-4 pb-4" },
+  _createClass(App, [{
+    key: "render",
+    value: function render() {
+      return React.createElement(
+        React.Fragment,
+        null,
         React.createElement(
-          "h3",
-          { className: "pb-3" },
-          "Intl.js support in this browser"
+          "div",
+          { className: "jumbotron pt-4 pb-4" },
+          React.createElement(
+            "h3",
+            { className: "pb-3" },
+            "Intl.js support in this browser"
+          ),
+          React.createElement(BrowserInfo, null)
         ),
-        React.createElement(BrowserInfo, null)
-      ),
-      React.createElement(
-        "div",
-        { className: "container" },
-        React.createElement(IntlJsSupport, { data: intljsTable.support })
-      ),
-      React.createElement("hr", null),
-      React.createElement(
-        "div",
-        { className: "container" },
-        React.createElement(NodejsInfo, { version: intljsTable.nodejs.version })
-      )
-    );
-  };
+        React.createElement(
+          "div",
+          { className: "container" },
+          React.createElement(IntlJsSupport, { data: this.state.intljs })
+        ),
+        React.createElement("hr", null),
+        React.createElement(
+          "div",
+          { className: "container" },
+          React.createElement(NodejsInfo, { version: this.state.nodejs.version })
+        )
+      );
+    }
+  }]);
 
-  ReactDOM.render(React.createElement(App, null), document.getElementById("root"));
-}
+  return App;
+}(React.Component);
+
+;
+
+ReactDOM.render(React.createElement(App, null), document.getElementById("root"));
